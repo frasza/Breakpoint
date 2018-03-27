b89817ae28aac112760194ee6f167d7adabb2e21
Zan Fras <zan.fraas@gmail.com>
2018-03-27 17:34:32 +0200

    create me vc

---
+++ Breakpoint/Controller/MeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MeViewController.swift
+//  Breakpoint
+//
+//  Created by Žan Fras on 27/03/2018.
+//  Copyright © 2018 Žan Fras. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+    
+    //MARK: - Outlets
+    /***************************************************************/
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    //MARK: - Methods
+    /***************************************************************/
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //MARK: - Actions
+    /***************************************************************/
+    @IBAction func signOutButtonPressed(_ sender: Any) {
+        
+    }
+    
+
+}
